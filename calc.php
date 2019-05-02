6327fbec9e8b94c89b02a30ab2d27f6c03ef7229
Shikhar-Batra <46950996+Shikhar-Batra@users.noreply.github.com>
2019-05-02 11:56:00 +0530

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,3 +1,14 @@
+<form method = "POST">
+    <input type="number" name="num1" required>
+    <select name="operations" required>
+        <option value="ADD">+</option>
+        <option value="SUB">-</option>
+        <option value="MUL">*</option>
+        <option value="DIV">%</option>
+    </select>
+    <input type="number" name="num2" required >
+    <input type="submit" value="Save" name="submit">
+</form>
 <?php
 include_once('test.php');
 
